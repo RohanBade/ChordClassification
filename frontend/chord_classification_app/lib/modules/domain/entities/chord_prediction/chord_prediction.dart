3fb438ac375897864bb65f371d90ae53bbb05c0f
Rissav25Pokhrel <risav.pok@gmail.com>
2025-01-03 14:11:06 +0545

    Added: Amplitude Graph , Continuous flow of graph, Speed Controller

--- frontend/chord_classification_app/lib/modules/domain/entities/chord_prediction/chord_prediction.dart
+++ frontend/chord_classification_app/lib/modules/domain/entities/chord_prediction/chord_prediction.dart
@@ -2,7 +2,22 @@ class ChordPrediction {
   int start;
   int end;
   String chord;
+  List<double>? amplitude;
 
   ChordPrediction(
-      {required this.start, required this.end, required this.chord});
+      {required this.start, required this.end, required this.chord,this.amplitude});
+
+  ChordPrediction copyWith({
+    int? start,
+    int? end,
+    String? chord,
+    List<double>? amplitude,
+  }) {
+    return ChordPrediction(
+      start:start ?? this.start,
+      end:end ?? this.end,
+      chord:chord ?? this.chord,
+      amplitude:amplitude ?? this.amplitude,
+    );
+  }
 }
